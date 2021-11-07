431888f84a67e15a5d829466cca93f0ba333f90b
IT23-2018-Lenka-Subotin <subotinlenka@gmail.com>
2021-11-07 22:31:32 +0100

    Revert " MINOR ISSUE 2"
    This reverts commit 29d31ca357f529346eb4f352880735f62b198739.

--- src/Business/Grand.Business.Authentication/Services/JwtBearerAuthenticationService.cs
+++ src/Business/Grand.Business.Authentication/Services/JwtBearerAuthenticationService.cs
@@ -12,8 +12,7 @@ namespace Grand.Business.Authentication.Services
         private readonly IUserApiService _userApiService;
 
         private string _errorMessage;
-        private string _email;
-
+       
         public JwtBearerAuthenticationService(
             ICustomerService customerService, IUserApiService userApiService)
         {
@@ -27,7 +26,7 @@ namespace Grand.Business.Authentication.Services
         /// <param name="customer">Customer</param>
         public virtual async Task<bool> Valid(TokenValidatedContext context)
         {
-            _email = context.Principal.Claims.ToList().FirstOrDefault(x => x.Type == "Email")?.Value;
+            var _email = context.Principal.Claims.ToList().FirstOrDefault(x => x.Type == "Email")?.Value;
             var token = context.Principal.Claims.ToList().FirstOrDefault(x => x.Type == "Token")?.Value;
             if (string.IsNullOrEmpty(token))
             {
