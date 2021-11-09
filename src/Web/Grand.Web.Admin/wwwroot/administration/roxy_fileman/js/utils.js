fc17dbf305b6135a5de6722c60d236b5cd18d558
jovanaostojic <jojka.ostojic99@gmail.com>
2021-11-09 13:25:54 +0100

    Blocker issues

--- src/Web/Grand.Web.Admin/wwwroot/administration/roxy_fileman/js/utils.js
+++ src/Web/Grand.Web.Admin/wwwroot/administration/roxy_fileman/js/utils.js
@@ -116,7 +116,7 @@ RoxyUtils.FileExists = function(path) {
   return ret;
 };
 RoxyUtils.GetFileIcon = function(path){
-  ret = 'images/filetypes/unknown.png';//'images/filetypes/file_extension_' + RoxyUtils.GetFileExt(path).toLowerCase() + '.png';
+  var ret = 'images/filetypes/unknown.png';//'images/filetypes/file_extension_' + RoxyUtils.GetFileExt(path).toLowerCase() + '.png';
   if(fileTypeIcons[RoxyUtils.GetFileExt(path).toLowerCase()]){
     ret = 'images/filetypes/' + fileTypeIcons[RoxyUtils.GetFileExt(path).toLowerCase()];
   }
